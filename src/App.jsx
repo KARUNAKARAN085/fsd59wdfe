07a0837c19598c2bb1df704e77ed89b80cff8346
KARUNAKARAN085 <karunakaranrk2001rk2001@gmail.com>
2024-06-30 21:22:03 +0530

    login page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Home from "./components/Home";
+import HomeWrapper from "./pages/HomeWrapper";
+import Register from "./components/Register";
+import Login from "./components/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeWrapper/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/register",
+        element: <Register />
+      },
+      {
+        path: "/login",
+        element: <Login />
+      }
+    ]
+  }
+]);
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  return <RouterProvider router={router}/>
 }
 
 export default App
